82567646a472e48b8646c61340a346d3f8bdae2f
Jacob Doherty <doherty.jacob.p@gmail.com>
2022-09-28 15:19:45 -0700

    build click event listener on todoEl updates compl

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // this will check if we have a user and set signout link if it exists
 import './auth/user.js';
 // Part A: import create todo
-import { createTodo, getTodos } from './fetch-utils.js';
+import { completeTodo, createTodo, getTodos } from './fetch-utils.js';
 // Part B: import get todos
 // Part C: import complete todos
 // Part D: import delete all function
@@ -21,15 +21,6 @@ let error = null;
 /* Events */
 
 window.addEventListener('load', async () => {
-    // > Part B: Add a click event listener for the todoEl
-    //      - call the async supabase function to delete all todos
-    //        and get the response
-    //      - set the todos and error state from the response
-    //      - if there's an error call displayError
-    //      - otherwise, display the todos
-
-    /* ^^^^^^^^^ I think these instructions are from the wrong step ^^^^^^^^^ */
-
     const response = await getTodos();
 
     error = response.error;
@@ -95,12 +86,25 @@ function displayTodos() {
         todoList.append(todoEl);
 
         // > Part C: Add a click event listener for the todoEl
-        //      - call the async supabase function to delete all todos
-        //        and get the response
-        //      - if there's an error, set error state and call displayError
-        //      - otherwise:
-        //          - find the index of todo in todos
-        //          - update that index of todos with the response data
-        //          - redisplay the todos
+        todoEl.addEventListener('click', async () => {
+            //      - call the async supabase function to delete all todos //?????
+            //        and get the response
+            const response = await completeTodo(todo.id);
+            error = response.error;
+            const completedTodo = response.data;
+            if (error) {
+                //      - if there's an error, set error state and call displayError
+                error = error.message;
+                displayError();
+                //      - otherwise:
+            } else {
+                //          - find the index of todo in todos
+                const index = todos.indexOf(todo);
+                //          - update that index of todos with the response data
+                todos[index] = completedTodo;
+                //          - redisplay the todos
+                displayTodos();
+            }
+        });
     }
 }
